c2f26875babf2c318008693b8438db4aa9e128c3
Michal Nowacki <nowa75@gmail.com>
2019-11-24 13:33:54 +0100

    complex config test

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -7,10 +7,14 @@
  */
 
 namespace KnpU\LoremIpsumBundle\Tests;
+
+use Exception;
 use KnpU\LoremIpsumBundle\KnpUIpsum;
 use KnpU\LoremIpsumBundle\KnpULoremIpsumBundle;
+use KnpU\LoremIpsumBundle\WordProviderInterface;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
@@ -18,20 +22,42 @@ class FunctionalTest extends TestCase
 {
     public function testServiceWiring()
     {
-        $kernel = new KnpULoremIpsumTestingKernel('test', true);
+        $kernel = new KnpULoremIpsumTestingKernel();
         $kernel->boot();
         $container = $kernel->getContainer();
 
         $ipsum = $container->get('knpu_lorem_ipsum.knpu_ipsum');
 
         $this->assertInstanceOf(KnpUIpsum::class, $ipsum);
-//        $this->assertInternalType('string',$ipsum->getParagraphs());
         $this->assertIsString($ipsum->getParagraphs());
+    }
+
+    public function testServiceWiringWithConfiguration()
+    {
+        $kernel = new KnpULoremIpsumTestingKernel([
+            'word_provider' => 'stub_word_list'
+        ]);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        $ipsum = $container->get('knpu_lorem_ipsum.knpu_ipsum');
+
+        $this->assertStringContainsString('stub',$ipsum->getWords(2));
     }
 }
 
 class KnpULoremIpsumTestingKernel extends Kernel
 {
+    /**
+     * @var array
+     */
+    private $knpUIpsumConfig;
+
+    public function __construct(array $knpUIpsumConfig = [])
+    {
+        $this->knpUIpsumConfig = $knpUIpsumConfig;
+        parent::__construct('test', true);
+    }
 
     /**
      * Returns an array of bundles to register.
@@ -47,9 +73,29 @@ class KnpULoremIpsumTestingKernel extends Kernel
 
     /**
      * Loads the container configuration.
+     * @param LoaderInterface $loader
+     * @throws Exception
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        // TODO: Implement registerContainerConfiguration() method.
+        $loader->load(function (ContainerBuilder $containerBuilder) {
+            $containerBuilder->register('stub_word_list', StubWordProvider::class);
+
+            $containerBuilder->loadFromExtension('knpu_lorem_ipsum', $this->knpUIpsumConfig);
+        });
+    }
+}
+
+class StubWordProvider implements WordProviderInterface
+{
+
+    /**
+     * Return an array of words to use for the fake text.
+     *
+     * @return array
+     */
+    public function getWordList(): array
+    {
+        return ['stub', 'stub2'];
     }
 }
